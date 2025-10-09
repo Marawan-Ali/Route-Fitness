278b55884fa33615409ef04da7add4bbed798fb4
Marawan Ali <marawanali190@gmail.com>
2025-10-09 09:54:15 +0300

    V6

---
+++ GymSystemDAL/Data/Configurations/MemberConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using GymSystemDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Data.Configurations
+{
+    internal class MemberConfiguration : GymUserConfiguration<Member>, IEntityTypeConfiguration<Member>
+    {
+        public new void Configure(EntityTypeBuilder<Member> builder)
+        {
+            // Special Type JoinDate
+            builder.Property(X => X.CreatedAt)
+                .HasColumnName("JoinDate")
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.Ignore(X => X.UpdatedAt);
+
+            base.Configure(builder);
+        }
+    }
+}
