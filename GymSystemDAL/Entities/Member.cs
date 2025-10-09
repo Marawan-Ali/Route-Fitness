a6f534e23d29b33f64929ed68f541396f0ac403e
Marawan Ali <marawanali190@gmail.com>
2025-10-09 09:25:02 +0300

    V5

---
+++ GymSystemDAL/Entities/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Entities
+{
+    internal class Member : GymUser
+    {
+        // CreatedAt Column in BaseEntity
+        // Will be used as JoinDate for Member => Configurations
+
+        public string? Photo { get; set; }
+    }
+}
---
+++ GymSystemDAL/Entities/Trainer.cs
@@ -0,0 +1,17 @@
+﻿using GymSystemDAL.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Entities
+{
+    internal class Trainer : GymUser
+    {
+        // CreatedAt Column in BaseEntity
+        // Will be used as HireDate for Trainer => Configurations
+
+        public Specialities Specialities { get; set; }
+    }
+}
