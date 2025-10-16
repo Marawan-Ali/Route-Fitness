1e5e33bb51c5bc1d184d07837ab920e62aac217f
Marawan Ali <marawanali190@gmail.com>
2025-10-16 09:33:21 +0300

    V2

---
+++ GymSystemBLL/Services/Classes/PlanService.cs
@@ -0,0 +1,56 @@
+﻿using GymSystemBLL.Services.Interfaces;
+using GymSystemBLL.ViewModels.PlanViewModels;
+using GymSystemDAL.Entities;
+using GymSystemDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemBLL.Services.Classes
+{
+    internal class PlanService : IPlanService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PlanService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<PlanViewModel> GetAllPlans()
+        {
+            var Plans = _unitOfWork.GetRepository<Plan>().GetAll();
+
+            if (Plans is null || !Plans.Any()) return [];
+
+            var PlanViewModels = Plans.Select(Plan => new PlanViewModel()
+            {
+                Id = Plan.Id,
+                Name = Plan.Name,
+                Description = Plan.Description,
+                DurationDays = Plan.DurationDays,
+                Price = Plan.Price,
+                IsActive = Plan.IsActive
+            });
+            return PlanViewModels;
+        }
+
+        public PlanViewModel? GetPlanById(int id)
+        {
+            var Plan = _unitOfWork.GetRepository<Plan>().GetById(id);
+
+            if (Plan is null) return null;
+            return new PlanViewModel()
+            {
+                Id = Plan.Id,
+                Name = Plan.Name,
+                Description = Plan.Description,
+                DurationDays = Plan.DurationDays,
+                Price = Plan.Price,
+                IsActive = Plan.IsActive
+            };
+        }
+    }
+}
---
+++ GymSystemBLL/Services/Interfaces/IPlanService.cs
@@ -0,0 +1,15 @@
+﻿using GymSystemBLL.ViewModels.PlanViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemBLL.Services.Interfaces
+{
+    internal interface IPlanService
+    {
+        IEnumerable<PlanViewModel> GetAllPlans();
+        PlanViewModel? GetPlanById(int id);
+    }
+}
