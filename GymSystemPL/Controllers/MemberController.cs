82751cc8172d0b582347548da04f51c2a4de66ae
Marawan Ali <marawanali190@gmail.com>
2025-10-23 12:00:08 +0300

    V4&5

--- GymSystemPL/Controllers/MemberController.cs
+++ GymSystemPL/Controllers/MemberController.cs
@@ -12,10 +12,52 @@ namespace GymSystemPL.Controllers
         {
             _memberService = memberService;
         } // Register Service in Program.cs
+
+        #region Get All Members
+
         public IActionResult Index()
         {
             var members = _memberService.GetAllMembers();
             return View(members);
         }
+
+        #endregion
+
+        #region Get Member Details
+
+        public ActionResult MemberDetails(int id)
+        {
+            if (id <=0)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var memberDetails = _memberService.GetMemberDetails(id);
+            if (memberDetails == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(memberDetails);
+        }
+
+        #endregion
+
+        #region Get Health Record Details
+
+        public ActionResult HealthRecordDetails(int id)
+        {
+            if (id <= 0)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            var healthRecordDetails = _memberService.GetMemberHealthRecordDetails(id);
+            if (healthRecordDetails == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(healthRecordDetails);
+        }
+
+        #endregion
     }
 }
