f53642d42f5a854b83d6af04b868e6add58bb019
Marawan Ali <marawanali190@gmail.com>
2025-10-27 11:31:01 +0300

    V6

---
+++ GymSystemPL/Controllers/SessionController.cs
@@ -0,0 +1,25 @@
+﻿using GymSystemBLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymSystemPL.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly ISessionService _sessionService;
+
+        public SessionController(ISessionService sessionService)
+        {
+            _sessionService = sessionService;
+        }
+
+        #region Get Sessions
+
+        public IActionResult Index()
+        {
+            var Sessions = _sessionService.GetAllSessions();
+            return View(Sessions);
+        }
+
+        #endregion
+    }
+}
